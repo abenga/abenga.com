a79506f3d9f2c5a2c5562b60588c6538a5263438
Horace <horace@abenga.com>
2020-12-27 23:01:51 +0300

    Add all struct fields

--- rs/abenga_site/src/lib/db/models/mod.rs
+++ rs/abenga_site/src/lib/db/models/mod.rs
@@ -1,4 +1,6 @@
 
+use uuid;
+
 
 #[derive(Clone, Debug, serde::Serialize)]
 pub struct Person {
@@ -30,24 +32,9 @@ pub struct LoginSession {
 
 
 #[derive(Clone, Debug, serde::Serialize)]
-pub struct Post {
-    pub id: i64,
-    pub title: &'static str,
-    pub joined_title: &'static str,
-    pub abstract_md: &'static str,
-    pub abstract_html: &'static str,
-    pub body_md: &'static str,
-    pub body_html: &'static str,
-    pub references_md: Option<&'static str>,
-    pub references_html: Option<&'static str>,
-    pub series_id: Option<i64>,
-    pub position_in_series: Option<i64>,
-}
-
-
-#[derive(Clone, Debug, serde::Serialize)]
 pub struct PostSeries {
     pub id: i64,
+    pub uid: &'static str,
     pub author_id: i64,
     pub title: &'static str,
     pub joined_title: &'static str,
@@ -55,8 +42,38 @@ pub struct PostSeries {
     pub abstract_html: &'static str,
     pub date_added: &'static str,
     pub last_edited: &'static str,
-    pub tags: Vec<i64>,
-    pub cover_image_path: &'static str,
-    // n_posts: i64,
-    // posts: HashMap<i64, Post>,
+    pub tags: Vec<String>,
+}
+
+
+#[derive(Clone, Debug, serde::Serialize)]
+pub struct Post {
+    pub id: i64,
+    pub uid: &'static str,
+    pub title: &'static str,
+    pub joined_title: &'static str,
+
+    pub date_added: &'static str,
+    pub last_edited: &'static str,
+
+    pub year_added: i64,
+    pub month_added: i64,
+    pub day_added: i64,
+
+    pub author_id: i64,
+
+    pub abstract_md: &'static str,
+    pub abstract_html: &'static str,
+
+    pub body_md: &'static str,
+    pub body_html: &'static str,
+
+    pub series_id: Option<i64>,
+    pub position_in_series: Option<i64>,
+
+
+    pub references_md: Option<&'static str>,
+    pub references_html: Option<&'static str>,
+
+    pub tags: Vec<String>,
 }
--- rs/abenga_site/src/lib/db/utils/posts/mod.rs
+++ rs/abenga_site/src/lib/db/utils/posts/mod.rs
@@ -6,22 +6,22 @@ use crate::lib::db::utils as db_utils;
 pub fn post_series() -> Vec<db_models::PostSeries> {
     let mut v = Vec::new();
     let mut conn = db_utils::get_connection().expect("Failure of database connection.");
-    for row in conn.query("SELECT id, title, joined_title, abstract_md, \
-                              abstract_html, body_md, body_html, references_md, \
-                              references_html, series_id, position_in_series FROM data.post_seriess;", &[]).unwrap() {
+    for row in conn.query("SELECT id, uid, author_id, title, joined_title, \
+    abstract_md, abstract_html, date_added, last_edited, tags \
+    FROM data.post_series;", &[]).unwrap() {
         let post_series = db_models::PostSeries {
             id: row.get(0),
-            author_id: row.get(1),
-            title: row.get(2),
-            joined_title: row.get(3),
-            abstract_md: row.get(4),
-            abstract_html: row.get(5),
-            date_added: row.get(6),
-            last_edited: row.get(7),
-            tags: row.get(8),
-            cover_image_path: row.get(8),
+            uid: row.get(1),
+            author_id: row.get(2),
+            title: row.get(3),
+            joined_title: row.get(4),
+            abstract_md: row.get(5),
+            abstract_html: row.get(6),
+            date_added: row.get(7),
+            last_edited: row.get(8),
+            tags: row.get(9),
         };
-        v.push(post.clone());
+        v.push(post_series.clone());
     }
     return v;
 }
@@ -30,21 +30,31 @@ pub fn post_series() -> Vec<db_models::PostSeries> {
 pub fn posts() -> Vec<db_models::Post> {
     let mut v = Vec::new();
     let mut conn = db_utils::get_connection().expect("Failure of database connection.");
-    for row in conn.query("SELECT id, title, joined_title, abstract_md, \
-                              abstract_html, body_md, body_html, references_md, \
-                              references_html, series_id, position_in_series FROM data.posts;", &[]).unwrap() {
+    for row in conn.query("SELECT \
+        id, uid, title, joined_title, date_added, last_edited, year_added, month_added, \
+        day_added, author_id, abstract_md, abstract_html, body_md, body_html, series_id, \
+        position_in_series, references_md, references_html, tags \
+        FROM data.posts;", &[]).unwrap() {
         let post = db_models::Post {
             id: row.get(0),
-            title: row.get(1),
-            joined_title: row.get(2),
-            abstract_md: row.get(3),
-            abstract_html: row.get(4),
-            body_md: row.get(5),
-            body_html: row.get(6),
-            references_md: row.get(7),
-            references_html: row.get(8),
-            series_id: row.get(9),
-            position_in_series: row.get(10),
+            uid: row.get(1),
+            title: row.get(2),
+            joined_title: row.get(3),
+            date_added: row.get(4),
+            last_edited: row.get(5),
+            year_added: row.get(6),
+            month_added: row.get(7),
+            day_added: row.get(8),
+            author_id: row.get(9),
+            abstract_md: row.get(10),
+            abstract_html: row.get(11),
+            body_md: row.get(12),
+            body_html: row.get(13),
+            series_id: row.get(14),
+            position_in_series: row.get(15),
+            references_md: row.get(16),
+            references_html: row.get(17),
+            tags: row.get(18),
         };
         v.push(post.clone());
     }
