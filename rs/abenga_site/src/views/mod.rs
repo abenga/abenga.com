61dd01ea9abcdb7c5fe3221fe8ea6ded614e5524
Horace <horace@abenga.com>
2021-01-17 00:34:44 +0300

    Show posts

--- rs/abenga_site/src/views/mod.rs
+++ rs/abenga_site/src/views/mod.rs
@@ -4,6 +4,9 @@
 use rocket_contrib::templates::Template;
 use rocket_contrib::serve::StaticFiles;
 
+use rocket_contrib::uuid::Uuid;
+use rocket_contrib::uuid::uuid_crate as uuid;
+
 use crate::lib::db::utils::posts as posts_utils;
 use crate::lib::db::models;
 
@@ -18,10 +21,20 @@ struct IndexTemplateContext {
 
 
 #[derive(serde::Serialize)]
-struct PostsTemplateContext<'a> {
+struct PostListTemplateContext<'a> {
     title: &'a str,
     name: &'a str,
     posts: Vec<models::Post>,
+    // This key tells handlebars which template is the parent.
+    parent: &'a str,
+}
+
+
+#[derive(serde::Serialize)]
+struct PostTemplateContext<'a> {
+    title: &'a str,
+    name: &'a str,
+    post: models::Post,
     // This key tells handlebars which template is the parent.
     parent: &'a str,
 }
@@ -44,13 +57,24 @@ pub fn index() -> Template {
 
 
 #[get("/posts")]
-pub fn posts() -> Template {
+pub fn show_recent_posts() -> Template {
     let _post_series = posts_utils::post_series();
-    let site_posts = posts_utils::posts();
-    Template::render("pages/posts", &PostsTemplateContext {
+    let most_recent_posts = posts_utils::posts();
+    Template::render("pages/posts", &PostListTemplateContext {
         title: "Posts",
         name: "Horace",
-        posts: site_posts,
+        posts: most_recent_posts,
+        parent: "base",
+    })
+}
+
+#[get("/post/<uid>")]
+pub fn show_post(uid: Uuid) -> Template {
+    let post = posts_utils::get_post(uid);
+    Template::render("pages/post", &PostTemplateContext {
+        title: "Posts",
+        name: "Horace",
+        post: post.unwrap(),
         parent: "base",
     })
 }
@@ -58,7 +82,7 @@ pub fn posts() -> Template {
 
 pub fn rocket() -> rocket::Rocket {
     rocket::ignite()
-        .mount("/", routes![index, posts])
+        .mount("/", routes![index, show_recent_posts, show_post])
         .mount("/static", StaticFiles::from("static"))
         .register(catchers![not_found])
         .attach(Template::fairing())
