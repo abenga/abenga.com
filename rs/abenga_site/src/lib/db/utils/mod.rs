97f18992ee5ebaa36e438eff8e7bd345d81e270e
Horace <horace@abenga.com>
2021-08-15 20:36:37 +0300

    Use remote database environment name by default

--- rs/abenga_site/src/lib/db/utils/mod.rs
+++ rs/abenga_site/src/lib/db/utils/mod.rs
@@ -7,14 +7,18 @@ use crate::lib::config;
 
 pub fn get_db_connection() -> Result<postgres::Client, postgres::Error> {
     let conf = config::get_config();
-    let db_env = env::var("DATABASE_ENV_NAME").expect("Database environment name not set!");
+    let db_env = if env::var("DATABASE_ENV_NAME").expect("Database environment name not set!") == "local" {
+        "local"
+    } else {
+        "remote"
+    };
     let connect_str = format!(
         "host={host} user={user} password={password} port={port} database={database}",
-        user=conf.databases[&db_env].user,
-        password=conf.databases[&db_env].password,
-        host=conf.databases[&db_env].host,
-        port=conf.databases[&db_env].port,
-        database=conf.databases[&db_env].database
+        user=conf.databases[db_env].user,
+        password=conf.databases[db_env].password,
+        host=conf.databases[db_env].host,
+        port=conf.databases[db_env].port,
+        database=conf.databases[db_env].database
     );
     let client = postgres::Client::connect(&connect_str, postgres::NoTls)?;
 
