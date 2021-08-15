f584847913f28fbaffc682cb29e091940a7a2442
Horace <horace@abenga.com>
2021-08-15 19:27:39 +0300

    Update to use new postgresql interface

--- rs/abenga_site/src/lib/db/utils/mod.rs
+++ rs/abenga_site/src/lib/db/utils/mod.rs
@@ -5,16 +5,18 @@ use std::env;
 use crate::lib::config;
 
 
-pub fn get_db_connection() -> Result<postgres::Connection, postgres::Error> {
+pub fn get_db_connection() -> Result<postgres::Client, postgres::Error> {
     let conf = config::get_config();
     let db_env = env::var("DATABASE_ENV_NAME").expect("Database environment name not set!");
-    let connect_str = format!("postgresql://{user}:{password}@{host}:{port}/{database}",
-                              user=conf.databases[&db_env].user,
-                              password=conf.databases[&db_env].password,
-                              host=conf.databases[&db_env].host,
-                              port=conf.databases[&db_env].port,
-                              database=conf.databases[&db_env].database);
-    let conn = postgres::Connection::connect(connect_str,
-                                             postgres::TlsMode::None).unwrap();
-    Ok(conn)
+    let connect_str = format!(
+        "host={host} user={user} password={password} port={port} database={database}",
+        user=conf.databases[&db_env].user,
+        password=conf.databases[&db_env].password,
+        host=conf.databases[&db_env].host,
+        port=conf.databases[&db_env].port,
+        database=conf.databases[&db_env].database
+    );
+    let client = postgres::Client::connect(&connect_str, postgres::NoTls)?;
+
+    Ok(client)
 }
