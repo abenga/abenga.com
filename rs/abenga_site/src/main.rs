1d1607b69dfce836fb373834bfa0e9c06f5ab442
Horace <horace@abenga.com>
2023-06-18 20:52:07 +0300

    Fetch port from environment variables

--- rs/abenga_site/src/main.rs
+++ rs/abenga_site/src/main.rs
@@ -23,9 +23,11 @@ async fn main() {
 
     // TODO: Config management can be fancier.
     let port: u16 = env::var("SITE_PORT")
-        .expect("ERR: port not found in env")
+        .unwrap_or_else(|_| String::from("8300"))
         .parse().expect("Invalid port number received!");
+
     let addr = SocketAddr::from(([127, 0, 0, 1], port));
+
     tracing::debug!("listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
